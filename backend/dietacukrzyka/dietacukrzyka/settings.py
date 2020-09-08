140ab556ebcbf2312ab800c82116edacf7c04c4c
yuriy.kis <yurakis9@gmail.com>
2020-09-08 21:48:57 +0200

    install-app-settings-py

--- backend/dietacukrzyka/dietacukrzyka/settings.py
+++ backend/dietacukrzyka/dietacukrzyka/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'zdrowadieta.apps.ZdrowadietaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
