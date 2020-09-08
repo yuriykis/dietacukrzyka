35578a1c67bcb6897b4823efe890048ebab3f104
yuriy.kis <yurakis9@gmail.com>
2020-09-08 21:25:28 +0200

    add-django-app

--- backend/dietacukrzyka/dietacukrzyka/settings.py
+++ backend/dietacukrzyka/dietacukrzyka/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+import os, sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+PROJECT_ROOT = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
