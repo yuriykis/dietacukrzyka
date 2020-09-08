a3b3dcfae03722608449d4cdfaa8e29f013d22c8
yuriy.kis <yurakis9@gmail.com>
2020-09-08 19:16:30 +0200

    update-left-menu

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -2,7 +2,7 @@
     <v-navigation-drawer
       v-model="drawer"
       :mini-variant.sync="mini"
-      color="#F3A9A0"
+      color="#F3D9B1"
     >
       <v-list-item class="px-2">
         <v-btn
@@ -10,24 +10,21 @@
           color="white"
           @click.stop="mini = !mini"
         >
-          <v-icon>mdi-chevron-right</v-icon>
+          <v-icon large>mdi-view-headline</v-icon>
         </v-btn>
       </v-list-item>
-
-      <v-divider></v-divider>
 
       <v-list dense>
         <v-list-item
           v-for="item in items"
           :key="item.title"
-          link
         >
-          <v-list-item-icon>
+          <v-list-item-icon class="my-5">
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
 
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          <v-list-item-content class="ma-0">
+            <v-btn>{{ item.title }}</v-btn>
           </v-list-item-content>
         </v-list-item>
       </v-list>
@@ -40,9 +37,9 @@
     data: () => ({
       drawer: true,
         items: [
-          { title: 'Home', icon: 'mdi-home-city' },
-          { title: 'My Account', icon: 'mdi-account' },
-          { title: 'Users', icon: 'mdi-account-group-outline' },
+          { title: 'Strona Domowa', icon: 'mdi-home-city' },
+          { title: 'Mój Profil', icon: 'mdi-account' },
+          { title: 'Przepisy', icon: 'mdi-food-apple' },
         ],
         mini: true,
     }),
