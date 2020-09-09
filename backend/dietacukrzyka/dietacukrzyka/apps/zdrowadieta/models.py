ec1c9d1b224de2e88abb0f79480d5b9f4728d068
yuriy.kis <yurakis9@gmail.com>
2020-09-09 20:51:36 +0200

    use-rest-serializer

--- backend/dietacukrzyka/dietacukrzyka/apps/zdrowadieta/models.py
+++ backend/dietacukrzyka/dietacukrzyka/apps/zdrowadieta/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
+    first_name = models.CharField("First Name", default="Yuriy", max_length=30)
+    last_name = models.CharField("Last Name", default="Kis", max_length=30)
 
     def __str__(self):
         return self.first_name + self.last_name
--- backend/dietacukrzyka/dietacukrzyka/apps/zdrowadieta/views.py
+++ backend/dietacukrzyka/dietacukrzyka/apps/zdrowadieta/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import permission_classes
+from rest_framework import permissions
 
 from .models import User
 from .serializers import UserSerializer
 
-
+@permission_classes((permissions.AllowAny,))
 class UsersView(APIView):
 
     def get(self, request):
-        users = User.objects.all()
+        users = User.objects.get(first_name="Yuriy")
         serializer = UserSerializer(users)
         return Response(serializer.data)
