c2e182806591257aa07af6fbf1661574d4ae383d
yuriy.kis <yurakis9@gmail.com>
2020-09-18 20:52:53 +0200

    set-titles-foodmenu

--- frontend/src/components/FoodMenu.vue
+++ frontend/src/components/FoodMenu.vue
@@ -11,13 +11,13 @@
             >
                 <v-row>
                     <v-col>
-                        <h3 class="ma-4">Dieta na ten tydzień</h3>
+                        <h2 class="ma-4">Dieta na ten tydzień</h2>
                     </v-col>
                     <v-col>
-                        <h5 class="ma-6">1500 kcal</h5>
+                        <h4 class="ma-6">1500 kcal</h4>
                     </v-col>
                     <v-col>
-                        <h5 class="ma-6">10.08.2020 - 16.08.2020</h5>
+                        <h4 class="ma-6">10.08.2020 - 16.08.2020</h4>
                     </v-col>
                 </v-row>
           </v-sheet>
@@ -31,12 +31,12 @@
           max-width="800"
           color= "rgba(28,29,30,0.8)"
         >
-        <v-row>
-            <v-col>
-                <h3 class="ml-5">{{ day }}</h3>
+        <v-row class="mb-2">
+            <v-col cols=3>
+                <h2 class="ml-5">{{ day }}</h2>
             </v-col>
             <v-col>
-              <h5 class="mt-4">{{i + 10}}.08.2020</h5>
+              <h4 class="mt-2">{{i + 10}}.08.2020</h4>
             </v-col>
         </v-row>
                 <v-slide-group
@@ -50,15 +50,15 @@
                   :center-active="centerActive"
                 >
                   <v-slide-item
-                    v-for="i in 9"
+                    v-for="i in 5"
                     :key="i"
                   >
                     <v-card
                       class="ma-4"
-                      height="100"
-                      width="200"
+                      height="200"
+                      width="300"
                     >
-                    <v-img @click="seeDetails()" :src="require(`../assets/image${i%3+1}.jpg`)"/>
+                    <v-img @click="seeDetails()" :src="require(`../assets/image${i%3+1}.jpg`)"><h3 class="ma-3"><span>{{ meal_types[i - 1] }}</span></h3></v-img>
                     </v-card>
                   </v-slide-item>
                </v-slide-group>
@@ -72,7 +72,8 @@
   export default {
     name: 'Menu',
     data: () => ({
-      days: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela']
+      days: ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'],
+      meal_types: ['Śniadanie', 'II śniadanie', 'Obiad', 'Podwieczorek', 'Kolacja']
     }),
     methods: {
       seeDetails () {
@@ -88,7 +89,11 @@
 .rounded-corner{
     border-radius:20px;
 }
-h3, h5 {
+h2, h4 {
+  color: white;
+}
+h3 span {
+  background-color: rgba(116,34,60,0.8);
   color: white;
 }
 </style>
