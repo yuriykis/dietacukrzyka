9158b4ffa3d9419911a9c40df300fdc72fd291ca
unknown <tomkielczewski@wp.pl>
2020-09-20 20:39:55 +0200

    Add images

--- frontend/src/components/DailyMenu.vue
+++ frontend/src/components/DailyMenu.vue
@@ -10,7 +10,7 @@
         >
         <v-row>
             <v-col>
-                <h3 class="ml-5"></h3>
+                <h2 class="ma-4">Poniedziałek </h2>
             </v-col>
             <v-col>
               <h5 class="mt-4"></h5>
@@ -28,12 +28,34 @@
           color= "rgba(28,29,30,0.8)"
         >
         <v-row>
-            <v-col>
-                <h3 class="ml-5">{{ meal }}</h3>
+            <v-col cols="6">
+               <v-row
+          :align='center'
+          :justify='end'
+          style="height: 300px;"
+        >
+                <h2 class="ml-5">{{ meal }}</h2>
+                  <v-img 
+
+                  
+
+                  @click="seeDetails()" :src="require(`../assets/image${i%3+1}.jpg`)">
+                    <h3 class="ma-3">
+                      {{ mealData[i].name }}
+                    </h3>
+                    <!-- <h4 class="ml-3">
+                      {{ mealData[i].ingedients }}
+                    </h4> -->
+                    <h4 class="ma-3">
+                      {{ mealData[i].method }}
+                    </h4>
+                  </v-img>
+              </v-row>
             </v-col>
             <v-col>
                 <h5 class="mt-4">{{ date }}</h5>
             </v-col>
+            
         </v-row>
             </v-sheet>
           </v-row>
@@ -42,13 +64,28 @@
 </template>
 
 <script>
+  import { getClientMenu } from '@/services/api'
   export default {
     name: 'Menu',
     data: () => ({
-      meals: ['Śniadanie', 'II śniadanie', 'Obiad', 'Podwieczorek', 'Kolacja']
+      meals: ['Śniadanie', 'II śniadanie', 'Obiad', 'Podwieczorek', 'Kolacja'],
+      mealData: []
     }),
     methods: {
-  
+      fetchData (i) {
+        
+        getClientMenu(i).then((response) => {
+          this.mealData.push(response.data)
+             if (i < 5) {
+                 this.fetchData(++i)
+             }
+          });
+
+      }
+    },
+    mounted () {
+        this.fetchData(0)
+        console.log(this.mealData)
     }
   }
 </script>
@@ -56,5 +93,16 @@
 <style scoped>
 .rounded-corner{
     border-radius:20px;
+}
+h2{
+  color: white;
+}
+h3 {
+  background-color:#A8C256;
+  color: white;
+}
+h4 {
+  background-color: rgba(116,34,60,0.8);
+  color: white;
 }
 </style>
