d2acbfb67e125d1d6ac6d69f35d353904c08f2fe
Yuriy Kis <54007065+yuriykis@users.noreply.github.com>
2020-11-12 23:13:12 +0100

    test-env-nginx

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import * as Axios from 'axios'
 import { getAccessToken } from '@/services/auth'
 
 
-var apiHost = 'https://' + process.env.PUBLIC_IP
+var apiHost = 'http://' + process.env.PUBLIC_IP
 if (process.env.NODE_ENV === 'development') {
   apiHost = 'http://localhost:8000'
 }
